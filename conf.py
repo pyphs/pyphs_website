41a01773bc6f936c0e478aa5bd824b8bb5d71789
Antoine FALAIZE <antoine.falaize@gmail.com>
2017-07-17 03:41:38 +0200

    Nikola auto commit.
    Nikola version: 7.8.9

--- conf.py
+++ conf.py
@@ -21,10 +21,10 @@ BLOG_AUTHOR = "afalaize"  # (translatable)
 BLOG_TITLE = "PyPHS"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://afalaize.github.io/pyphs/"
+SITE_URL = "https://pyphs.github.io/pyphs/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
-# BASE_URL = "https://afalaize.github.io/pyphs/"
+# BASE_URL = "https://pyphs.github.io/pyphs/"
 BLOG_EMAIL = "antoine.falaize@gmail.com"
 BLOG_DESCRIPTION = "This is a companion site for the python package PyPHS"  # (translatable)
 
